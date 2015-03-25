0f4c15793b658647c7dbc9b98e651f804469e327
Mark Farrell <m4farrel@uwaterloo.ca>
2015-03-25 11:31:05 -0300

    UTM route.
    Signed-off-by: Mark Farrell <m4farrel@uwaterloo.ca>

--- elevation.rkt
+++ elevation.rkt
@@ -14,6 +14,7 @@
 (require file/convertible)
 (require web-server/servlet)
 (require (planet dmac/spin))
+(require data/utm)
 
 ;; (: data-dir String)
 (define data-dir
@@ -75,7 +76,7 @@
 
 ;; (: SRTM->png-file (-> SRTM String))
 (define (SRTM->png-file srtm)
-  (let ([file-name-prefix (list->string (drop (string->list (SRTM-file-name srtm)) 4))])
+  (let ([file-name-prefix (list->string (drop-right (string->list (SRTM-file-name srtm)) 4))])
     (begin
       (printf "Translating GeoTiff file to PNG.\n")
       (time (system (string-append "gdal_translate "
@@ -89,17 +90,57 @@
 
 ;; (: SRTM->xyz-file (-> SRTM String))
 (define (SRTM->xyz-file srtm)
-  (let ([file-name-prefix (list->string (drop (string->list (SRTM-file-name srtm)) 4))])
+  (let ([file-name-prefix (list->string (drop-right (string->list (SRTM-file-name srtm)) 4))])
     (begin
       (printf "Translating GeoTiff file to XYZ.\n")
-      (time (system (string-append "gdal_translate "
-                                   ""
+      (time (system (string-append "gdal_translate"
+                                   " "
                                    "-of XYZ "
                                    file-name-prefix
                                    ".tif "
                                    file-name-prefix
                                    ".xyz")))
       (string-append file-name-prefix ".xyz"))))
+
+;; (: SRTM-utm-project (-> SRTM SRTM))
+(define (SRTM-utm-project srtm)
+  (let* ([file-name-prefix (list->string (drop-right (string->list (SRTM-file-name srtm)) 4))]
+         [new-file-name (string-append file-name-prefix "_utm.tif")]
+         [zone (utm-zone (* 0.5
+                            (+ (SRTM-min-long srtm)
+                               (SRTM-max-long srtm)))
+                         (* 0.5
+                            (+ (SRTM-min-lat srtm)
+                               (SRTM-max-lat srtm))))])
+    (begin
+      (printf "UTM-projecting GeoTiff file.~n")
+      (printf "Calculated UTM zone: ~a~n" zone)
+      (when (file-exists? new-file-name)
+        (begin
+          (printf "Removing old UTM-projected GeoTiff file.~n")
+          (time (system (string-append "rm -f " new-file-name)))))
+      (time (system (string-append "gdalwarp"
+                                   " "
+                                   (SRTM-file-name srtm)
+                                   " "
+                                   new-file-name
+                                   " "
+                                   "-q"
+                                   " "
+                                   "-t_srs "
+                                   "\""
+                                   "+proj=utm"
+                                   " "
+                                   "+zone="
+                                   (number->string zone)
+                                   " "
+                                   "+ellps=WGS84"
+                                   "\"")))
+      (SRTM new-file-name
+            (SRTM-min-long srtm)
+            (SRTM-min-lat  srtm)
+            (SRTM-max-long srtm)
+            (SRTM-max-lat  srtm)))))
 
 ;; (: SRTM-intersection (-> (Listof SRTM) Real Real Real Real SRTM))
 (define (SRTM-intersection srtms min-long min-lat max-long max-lat)
@@ -114,14 +155,14 @@
       (printf "\n")
       (printf "Downloading ~a tiles of elevation data.\n"
               (length srtms))
-      (for/list ([srtm srtms])
-                (begin (printf "Downloading ~s.\n"
-                               (SRTM-file-name srtm))
-                       (time
-                         (unless (file-exists? (string-append data-dir
-                                                              (SRTM-file-name srtm)
-                                                              ".tif"))
-                           (SRTM-download srtm)))))
+      (for ([srtm srtms])
+        (begin (printf "Downloading ~s.\n"
+                       (SRTM-file-name srtm))
+               (time
+                 (unless (file-exists? (string-append data-dir
+                                                      (SRTM-file-name srtm)
+                                                      ".tif"))
+                   (SRTM-download srtm)))))
       (printf "Merging tiles together.\n")
       (time (system (string-append "gdal_merge.py "
                                    "-q "
@@ -205,6 +246,15 @@
                                 (string->number (params req 'maxlong))
                                 (string->number (params req 'maxlat)))))
               #"text/plain")
+    (file-get "/utm"
+              (λ (req)
+                 (SRTM->png-file
+                   (SRTM-utm-project
+                     (SRTM-inside (string->number (params req 'minlong))
+                                  (string->number (params req 'minlat))
+                                  (string->number (params req 'maxlong))
+                                  (string->number (params req 'maxlat))))))
+              #"image/png")
     (run)))
 
 ;; Entry point:
