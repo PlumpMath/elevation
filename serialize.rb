f4f324fabfe0167e69d0912e6cf17a2bc33b08d9
Mark Farrell <markfarr2011@gmail.com>
2015-04-27 15:41:42 -0300

    Update serialize.rb

--- serialize.rb
+++ serialize.rb
@@ -6,14 +6,6 @@ require 'rdf/ntriples'
 require 'rdf/n3'
 require 'rdf/raptor'
 
-# Dependencies:
-# sudo gem install uuid rdf rdf-raptor
-
-# e.g.
-# $ cat input.xyz
-# 2.5 50.3 100.3
-# $ ./serialize < input.xyz
-
 md5 = Digest::MD5.new
 
 ARGF.each_line { |line|
@@ -37,8 +29,3 @@ ARGF.each_line { |line|
   $stdout.write output
 
 }
-
-# e.g.
-#_:a8587640-ccc4-0132-1754-2c87a3092396 <http://www.w3.org/2003/01/geo/wgs84_pos#long> "2.5" .
-#_:a8587640-ccc4-0132-1754-2c87a3092396 <http://www.w3.org/2003/01/geo/wgs84_pos#lat> "50.3" .
-#_:a8587640-ccc4-0132-1754-2c87a3092396 <http://www.w3.org/2003/01/geo/wgs84_pos#alt> "100.3" .
